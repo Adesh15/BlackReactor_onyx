59bcc57b03431163233f6e111cd02f62a64bbc83
adesh15 <adesikha15@gmail.com>
2017-05-16 05:54:23 +0200

    Revert "Add and Enable Wheatley and Smartmax gov."
    This reverts commit 854bd014413e7e60b18fd80d30266859b79871c9.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -383,18 +383,14 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
-extern struct cpufreq_governor cpufreq_gov_wheatley;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
-extern struct cpufreq_governor cpufreq_gov_smartmax;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_REACTIVE)
 extern struct cpufreq_governor cpufreq_gov_reactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_reactive)
+=======
+>>>>>>> parent of 854bd01... Add and Enable Wheatley and Smartmax gov.
 #endif
 
 
